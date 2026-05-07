409a17421366fbe9d1673186f7ad9efcc9ace7de
Kyle Cesare <kcesare@gmail.com>
2026-05-07 16:44:08 -0600

    Simplify cantor pairing calculation.

--- src/util.rs
+++ src/util.rs
@@ -2,17 +2,17 @@ use glamx::DVec3;
 
 // TODO: May break with big numbers due to checked arithmetic.
 // TODO: unsigned ints?
-pub fn cantor_pairing(k1: i64, k2: i64) -> i64 {
-    i64::wrapping_mul((k1 + k2) / 2, (k1 + k2 + 1) + k2)
+pub fn cantor_pairing(a: i64, b: i64) -> i64 {
+    ((a + b) * (a + b + 1) / 2) + b
 }
 
 // TODO: tol acts like a diameter, allowing for 0.5 * tol on either side.
-pub fn cantor_point_hash(point: DVec3, tol: f64) -> i64 {
-    let x = (point.x / tol).round() as i64;
-    let y = (point.y / tol).round() as i64;
-    let z = (point.z / tol).round() as i64;
+pub fn cantor_point_hash(v: DVec3, tol: f64) -> i64 {
+    let qx = (v.x / tol).round() as i64;
+    let qy = (v.y / tol).round() as i64;
+    let qz = (v.z / tol).round() as i64;
 
-    cantor_pairing(x, cantor_pairing(y, z))
+    cantor_pairing(cantor_pairing(qx, qy), qz)
 }
 
 #[cfg(test)]
@@ -37,6 +37,10 @@ mod tests {
         assert_eq!(
             cantor_point_hash(DVec3::new(-1.0, 2.0, -1.04), tol),
             cantor_point_hash(DVec3::new(-1.0, 2.0, -1.0), tol)
+        );
+        assert_ne!(
+            cantor_point_hash(DVec3::new(1.0, 2.0, 3.0), tol),
+            cantor_point_hash(DVec3::new(-1.0, -2.0, -3.0), tol)
         );
         assert_ne!(
             cantor_point_hash(DVec3::new(1.0, 2.0, 3.5), tol),
